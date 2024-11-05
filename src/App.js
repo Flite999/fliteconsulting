0550111445dffe4b2686cd7e84385115a295281b
Mark <17260591+MarkKleinhaus@users.noreply.github.com>
2024-11-05 14:15:45 -0800

    update design

--- src/App.js
+++ src/App.js
@@ -17,21 +17,22 @@ function App() {
 
     return (
         <div className={`App ${contactFormOpen ? 'no-interaction' : ''}`}>
-            <div className="company-title">
-                <h1>Flite Consulting</h1>
-                <p>Software consulting services and projects</p>
-                <ul>
-                    <li>Mobile Apps</li>
-                    <li>Software Architecture</li>
-                    <li>Help your services scale and be reliable</li>
-                </ul>
+            <div className={`app-content ${menuOpen ? 'shifted' : ''}`}>
+                <div className="company-title">
+                    <h1>Flite Consulting</h1>
+                    <p>Software consulting services and projects</p>
+                    <ul>
+                        <li>Mobile Apps</li>
+                        <li>Software Architecture</li>
+                        <li>Help your services scale and be reliable</li>
+                    </ul>
+                </div>
+                <button className="menu-button" onClick={toggleMenu}>
+                    &#9776;
+                </button>
+
+
             </div>
-            <button className="menu-button" onClick={toggleMenu}>
-                &#9776;
-            </button>
-
-            {menuOpen && <PortfolioMenu />}
-
             <footer className="footer">
                 <p>
                     <a href="#contact-form" onClick={toggleContactForm}>Contact</a> for a free consultation and quote today.
@@ -39,8 +40,9 @@ function App() {
                 <p>&copy; 2024 Flite Consulting. All rights reserved.</p>
             </footer>
 
-            {contactFormOpen && <ContactForm toggleContactForm={toggleContactForm} />}
+            <PortfolioMenu className={menuOpen ? 'open' : ''} />
 
+            {contactFormOpen && <ContactForm toggleContactForm={toggleContactForm} />}
         </div>
     );
 }
--- src/PortfolioMenu.js
+++ src/PortfolioMenu.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './PortfolioMenu.css'; // Separate CSS for styling
 
-function PortfolioMenu() {
+function PortfolioMenu({ className }) {
     return (
-        <div className="portfolio-menu">
+        <div className={`portfolio-sidebar ${className}`}>
             <div>
                 <h2>Portfolio</h2>
-                <p> (under construction)</p>
+                <p><a href="https://www.markkleinhaus.com" target="_blank" rel="noopener noreferrer">Current & Past Projects</a></p>
             </div>
             <a href="https://www.linkedin.com/in/markkleinhaus/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin fa-xl"></i>
